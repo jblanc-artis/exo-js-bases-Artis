237613a36f56dbdf5116438ab179fb348a86757a
jojo <jblanc@simplon.co>
2021-01-24 18:22:00 +0100

    second commit

--- app.js
+++ app.js
@@ -74,3 +74,16 @@ addToDoButton.addEventListener('click', () => {
 		toDoContainer.removeChild(paragraph);
 	});
 });
+
+// EXO 5
+window.addEventListener('load', () => {
+	const sounds = document.querySelectorAll('.sound');
+	const pads = document.querySelectorAll('.pads div');
+
+	pads.forEach((pad, index) => {
+		pad.addEventListener('click', function() {
+			sounds[index].currentTime = 0;
+			sounds[index].play();
+		});
+	});
+});
